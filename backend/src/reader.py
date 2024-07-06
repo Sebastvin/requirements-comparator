189bd07606ed50d8f0757984071e97402f4996c1
Sebastvin <Sebastvin@gmail.com>
2024-07-06 20:08:29 +0200

    Update file structure, add basics to backend

---
+++ backend/src/reader.py
@@ -0,0 +1,34 @@
+from typing import List
+from dataclasses import dataclass
+from backend.src.models.schemas import Package
+
+
+@dataclass
+class ContextManager:
+    path: str
+
+    def read_file(self) -> List[List[str]]:
+        try:
+            with open(self.path, "r") as file:
+                return list(map(self.process_line, file.readlines()))
+        except IOError as e:
+            print(f"An error occurred while reading the file: {e}")
+            return []
+
+    def process_line(self, line: str) -> Package:
+        line = line.strip()
+        if not line or line.startswith("#"):
+            return None
+
+        # Handle Git repository packages
+        if "@" in line and "git+" in line:
+            parts = line.split("@")
+            name = parts[0].strip()
+            return Package(name=name, version="")
+
+        # Handle regular packages with versions
+        if "==" in line:
+            name, version = line.split("==")
+            return Package(name=name.strip(), version=version.strip())
+
+        return Package(name=line, version="")
